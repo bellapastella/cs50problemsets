826c23cbb987e8e0e906dd4a5b9e3e4d3429a4e5
bellapastella <158636688+bellapastella@users.noreply.github.com>
2024-02-03 00:22:47 +0000

    Fixed the Einstein problem.

--- PS0/einstein.py
+++ PS0/einstein.py
@@ -1,13 +1,6 @@
-# mass = int(input("Provide a Mass: "))
-
-# # e = mc^2
-# energy = mass * 300000000 ** 2
-
 def energy(mass):
-energy = mass * 300000000 ** 2
-return energy
+  energy = mass * 300000000 ** 2
+  return energy
 
-# print(energy)
 mass = int(input("Provide a Mass: "))
 print(energy(5))
-
